9be17ef4109df9f6e1f958eafe38aa8196bdd56a
Mateusz Wieczorek <114943990+Mateusz-best-creator@users.noreply.github.com>
2023-11-03 19:30:21 +0100

    Update nds.cpp

--- TreasureWorkshop/nds.cpp
+++ TreasureWorkshop/nds.cpp
@@ -7,23 +7,23 @@
 using namespace std;
 
 // Reset the entire game
-void NDS_command(vector<Player *> &players,
-                 vector<int> &trapdoor_coordinates_x, vector<int> &trapdoor_coordinates_y,
-                 vector<int> &treasure_coordinates_x, vector<int> &treasure_coordinates_y,
-                 vector<int> &walls_coordinates_x, vector<int> &walls_coordinates_y)
+void NDS_command(vector<Player*>& players,
+	vector<int>& trapdoor_coordinates_x, vector<int>& trapdoor_coordinates_y,
+	vector<int>& treasure_coordinates_x, vector<int>& treasure_coordinates_y,
+	vector<int>& walls_coordinates_x, vector<int>& walls_coordinates_y)
 {
-    // Free the memory
-    for (size_t i = 0; i < players.size(); ++i)
-    {
-        delete players[i];
-    }
+	// Free the memory
+	for (size_t i = 0; i < players.size(); ++i)
+	{
+		delete players[i];
+	}
 
-    // Clear all vectors
-    players.clear();
-    trapdoor_coordinates_x.clear();
-    trapdoor_coordinates_y.clear();
-    treasure_coordinates_x.clear();
-    treasure_coordinates_y.clear();
-    walls_coordinates_x.clear();
-    walls_coordinates_y.clear();
+	// Clear all vectors
+	players.clear();
+	trapdoor_coordinates_x.clear();
+	trapdoor_coordinates_y.clear();
+	treasure_coordinates_x.clear();
+	treasure_coordinates_y.clear();
+	walls_coordinates_x.clear();
+	walls_coordinates_y.clear();
 }
