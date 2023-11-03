d640dc60efe8c06b4fedef9201234969ccf3364f
Mateusz Wieczorek <114943990+Mateusz-best-creator@users.noreply.github.com>
2023-11-03 19:29:06 +0100

    Update add_object.cpp

--- TreasureWorkshop/add_object.cpp
+++ TreasureWorkshop/add_object.cpp
@@ -6,17 +6,17 @@ using namespace std;
 
 const static int MAX_NUMBER_OF_OBJECTS = 100;
 
-void add_object_to_game(vector<int> &coordinates_x, vector<int> &coordinates_y, const string &object_name)
+void add_object_to_game(vector<int>& coordinates_x, vector<int>& coordinates_y, const string& object_name)
 {
-    if (coordinates_x.size() > MAX_NUMBER_OF_OBJECTS)
-    {
-        cout << "Maximum number of " << object_name << " exceeded." << endl;
-    }
-    else
-    {
-        int new_x, new_y;
-        cin >> new_x >> new_y;
-        coordinates_x.push_back(new_x);
-        coordinates_y.push_back(new_y);
-    }
+	if (coordinates_x.size() > MAX_NUMBER_OF_OBJECTS)
+	{
+		cout << "Maximum number of " << object_name << " exceeded." << endl;
+	}
+	else
+	{
+		int new_x, new_y;
+		cin >> new_x >> new_y;
+		coordinates_x.push_back(new_x);
+		coordinates_y.push_back(new_y);
+	}
 }
